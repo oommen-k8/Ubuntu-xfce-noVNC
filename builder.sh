acea8094b24c1ff66064003fbf7c17ddfdae2e0b
oommen-k8 <142365164+oommen-k8@users.noreply.github.com>
2023-09-06 19:59:47 -0400

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -4,6 +4,10 @@ echo what name do you what your image to be
 
 read name
 
+echo do you want to run this in background -d or foreground -it ?
+
+read run
+
 echo building docker image
 
 docker build -t $name .
@@ -12,4 +16,4 @@ sleep 3
 
 echo running docker image
 
-docker run -it -p 5901:5901 -p 6080:6080 $name          
+docker run $run -p 5901:5901 -p 6080:6080 $name          
