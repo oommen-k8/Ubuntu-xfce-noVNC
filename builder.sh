c80748f6db186369308c76aa7f90ca9c3eba7405
oommen-k8 <142365164+oommen-k8@users.noreply.github.com>
2023-09-06 17:29:45 -0400

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -1,13 +1,15 @@
 !#/bin/bash
 
-sleep 3
+echo what name do you what your image to be
+
+read name
 
 echo building docker image
 
-docker build -t ubuntuxfce .
+docker build -t $name .
 
 sleep 3
 
 echo running docker image
 
-docker run -it -p 5901:5901 -p 6080:6080 ubuntuxfce          
+docker run -it -p 5901:5901 -p 6080:6080 $name          
