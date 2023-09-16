7c4a34d4fb62335776ff303a938581d4ca1b76cb
oommen-k8 <142365164+oommen-k8@users.noreply.github.com>
2023-09-15 20:59:54 -0400

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -8,15 +8,11 @@ echo do you want to run this in background -d or foreground -it ?
 
 read run
 
-echo what image more apps or basic . mean basic ./More apps/dockerfile is more apps
-
-read path
-
 echo building docker image
 
 docker pull oommen81/xfce:xfce-only
 
-docker build -t $name $path
+docker build -t $name .
 
 sleep 3
 
